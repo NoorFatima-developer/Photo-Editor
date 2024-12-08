bdf19bd8ada79f253445a691ab3411933677d7ca
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-12-08 17:42:36 +0500

    code is added

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 const fileInput = document.querySelector('.file-input');
 const chooseImgBtn = document.querySelector('.choose-img');
+const previewImg = document.querySelector('.preview-img');
+
+const loadImage = () => {
+    const file = fileInput.files[0];
+    if(!file) return; //return if user hasn't selected file...
+    console.log(file);
+}
+
+
+fileInput.addEventListener('change', loadImage)
+chooseImgBtn.addEventListener('click', () => {
+    fileInput.click();
+  });
+  
