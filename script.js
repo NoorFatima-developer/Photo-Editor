c3e6889f9b715a3b532588022a877fbc0e85b73f
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-12-08 23:09:01 +0500

    code is added

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const fileInput = document.querySelector('.file-input');
 const chooseImgBtn = document.querySelector('.choose-img');
 const previewImg = document.querySelector('.preview-img img');
-const filterOptions = document.querySelector('.filter button');
+const filterOptions = document.querySelectorAll('.filter .options button');
 const loadImage = () => {
     const file = fileInput.files[0];
     if(!file) return; //return if user hasn't selected file...
@@ -12,14 +12,21 @@ const loadImage = () => {
     })
 }
 
-filterOptions.forEach((option) => {
-    option.addEventListener()
-})
+filterOptions.forEach(options => {
+    options.addEventListener('click', () => {
+        document.querySelector(".filter .active").classList.remove('active');
+        options.classList.add('active');
+    })
+    console.log(options);
+    
+});
 
 fileInput.addEventListener('change', loadImage)
 chooseImgBtn.addEventListener('click', () => {
     fileInput.click();
 });
+
+
 
 
 
