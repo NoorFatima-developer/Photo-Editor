ad15495eb36ca5ad1739e1ca27142bc80cc6af35
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-12-08 17:48:06 +0500

    code is added

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const fileInput = document.querySelector('.file-input');
 const chooseImgBtn = document.querySelector('.choose-img');
-const previewImg = document.querySelector('.preview-img');
+const previewImg = document.querySelector('.preview-img img');
 
 const loadImage = () => {
     const file = fileInput.files[0];
     if(!file) return; //return if user hasn't selected file...
-    console.log(file);
+    // console.log(file);
+    previewImg.src = URL.createObjectURL(file);
 }
 
 
